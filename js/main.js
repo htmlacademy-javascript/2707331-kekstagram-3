64e8659a3568e67f4bdb464b16c078b8c3196895
setpolet <setpolet@gmail.com>
2026-09-01 22:57:58 +0300

    Add data loading error handling

--- js/main.js
+++ js/main.js
@@ -2,7 +2,25 @@ import './form.js';
 import { renderPictures } from './pictures.js';
 import { getData } from './api.js';
 
+const dataErrorTemplate = document
+  .querySelector('#data-error')
+  .content
+  .querySelector('.data-error');
+
+const showDataError = () => {
+  const dataError = dataErrorTemplate.cloneNode(true);
+
+  document.body.append(dataError);
+
+  setTimeout(() => {
+    dataError.remove();
+  }, 5000);
+};
+
 getData()
   .then((photos) => {
     renderPictures(photos);
+  })
+  .catch(() => {
+    showDataError();
   });
