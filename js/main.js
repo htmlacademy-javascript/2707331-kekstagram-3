2f6a0b47836d0ccd36ea34cc90c0eabffbbaabe8
setpolet <setpolet@gmail.com>
2026-09-03 18:30:39 +0300

    Extract data error handling

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
 import './form.js';
 import { renderPictures } from './pictures.js';
 import { getData } from './api.js';
-
-const dataErrorTemplate = document
-  .querySelector('#data-error')
-  .content
-  .querySelector('.data-error');
-
-const showDataError = () => {
-  const dataError = dataErrorTemplate.cloneNode(true);
-
-  document.body.append(dataError);
-
-  setTimeout(() => {
-    dataError.remove();
-  }, 5000);
-};
+import { showDataError } from './data-error.js';
 
 getData()
   .then((photos) => {
