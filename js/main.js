109e66ffe59bdd0dd26514bd16ec39b099cbbd23
setpolet <setpolet@gmail.com>
2026-09-04 21:10:02 +0300

    Show photo filters after data loading

--- js/main.js
+++ js/main.js
@@ -3,9 +3,12 @@ import { renderPictures } from './pictures.js';
 import { getData } from './api.js';
 import { showDataError } from './data-error.js';
 
+const imgFilters = document.querySelector('.img-filters');
+
 getData()
   .then((photos) => {
     renderPictures(photos);
+    imgFilters.classList.remove('img-filters--inactive');
   })
   .catch(() => {
     showDataError();
